ba1f016525ecb759920eeb2c0b4c84b18cb8b438
jantonio170 <jantonio_170b@hotmail.com>
2018-04-26 15:56:59 +0000

    Se agrego parametros al constructor y validaciones

--- Ejemplo-1-calentador.cpp
+++ Ejemplo-1-calentador.cpp
@@ -1,40 +1,54 @@
 #include<iostream>
-
+//Documentacion cppreference.com
 class Calentador
 {
 private: 
     int temperatura;
     int incremento;
+    int min;
+    int max;
 public:
-    Calentador();
+    Calentador(int min, int max, int temperatura = 0);//Constructor
     void calentar();
     void enfriar();
-    void imprimeteTemperatura();
+    void imprimeCentigrados() const;
     void imprimeFarenheit() const;
     int accedeTemperatura() const; //Metodo de Acceso y se le agrega la palabra const para saber que el metodo no modifica las variables de instancia
 }; //punto y coma obligatorio
 
-Calentador::Calentador()
+Calentador::Calentador(int min, int max, int temperatura)
 {
-    temperatura = 15;
+    if(max < min){
+       std:: cout << "Error en el rango min-max" << std::endl;
+       std::exit(EXIT_FAILURE);
+       // throw "Error en el rango min-max"; //La palabra throw indica errores a los metodos que la llamaron
+    }
+    if(temperatura >= min && temperatura <=max){
+         this->temperatura = temperatura;
+    }
+    else{
+        this->temperatura = min;
+    }
     incremento = 3;
+    this->min = min; //this es un apuntador y por eso lleva flechita ->
+    this->max = max;
 }
 
 void Calentador::calentar()
 {
-    if(temperatura + incremento <= 30) {
+    if(temperatura + incremento <= this->max) {
         temperatura +=incremento;
     }
 }
 
 void Calentador::enfriar()
 {
-    if(temperatura - incremento >= -10) {
+    if(temperatura - incremento >= this->min) {
         temperatura -=incremento;
     }
 }
 
-void Calentador::imprimeteTemperatura()
+void Calentador::imprimeCentigrados() const
 {
     std::cout << "La temperatura es " << temperatura << "\n"; //Salto de linea std::endl o "/n"
 }
@@ -52,15 +66,19 @@ void Calentador::imprimeFarenheit() const
 
 int main()
 {
-    Calentador c1;
-    Calentador c2;
+    //try {
+    Calentador c1{-10, 10}; //Lleva llaves para darle los parametros
+    Calentador c2{30, 10, 10};
     
     c1.calentar();
-    c1.imprimeteTemperatura();
+    c1.imprimeCentigrados();
     c1.imprimeFarenheit();
     
     c2.enfriar();
-    c2.imprimeteTemperatura();
+    c2.imprimeCentigrados();
     c2.imprimeFarenheit();
-
+  //  }
+   // catch (const std::runtime_error &e){
+        
+    //}
 }
